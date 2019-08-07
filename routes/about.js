a6f0574fdf4ab8848187b0d4cfd08d63a0c04ec5
Joshua Abu <joshujah007@gmail.com>
2019-08-07 19:05:00 -0400

    base route and setup

--- routes/about.js
+++ routes/about.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const aboutRoute = express.Router();
 
-aboutRoute.get('/something', (req, res ) => {
-  res.send('something subroute');
-})
-
 aboutRoute.get('/', ( req, res ) => {
-  res.send('index of about')
-})
+  res.send('main about page')
+});
+
+aboutRoute.get('/something', (req, res ) => {
+  res.send('about subroute');
+});
 
 
 module.exports = aboutRoute;
